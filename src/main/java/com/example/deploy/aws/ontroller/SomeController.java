0a16c5c2f2dd468847e8b2a6d323d51e0ee9d881
saibal chakraborty <saibal183@gmail.com>
2021-07-10 13:17:20 +0530

    Initial Commit for testing aws deployment

---
+++ src/main/java/com/example/deploy/aws/ontroller/SomeController.java
@@ -0,0 +1,16 @@
+package com.example.deploy.aws.ontroller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/aws")
+public class SomeController {
+	
+	@GetMapping(value = "/hello")
+	public String sayHelloToAws() {
+		return "Hello Saibal!!";
+	}
+
+}
